a223f4dbc2176ef48a27a8182fc21ddbe2495615
ghkdwjdgk123 <ghkdwjdgk123@gmail.com>
2024-07-05 09:35:54 +0900

    :art: 2024-07-01 회원 가입 기능 수정

--- src/main/java/team/cheese/controller/ResisterController.java
+++ src/main/java/team/cheese/controller/ResisterController.java
@@ -13,8 +13,9 @@ import team.cheese.domain.UserDto;
 import team.cheese.service.AddrCdService;
 import team.cheese.service.AdminService;
 import team.cheese.service.UserService;
-
+import team.cheese.entity.UserVaildation;
 import javax.validation.Valid;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Timestamp;
 import java.util.List;
 
@@ -45,25 +46,15 @@ public class ResisterController {
 
     @PostMapping("/createAccount")
     @Validated
-    public String createAccount(@ModelAttribute @Valid UserDto userDto, BindingResult result, String addr_cd, String addr_nm) throws Exception {
-        if(result.hasErrors()) {
+    public String createAccount(@ModelAttribute @Valid UserDto userDto, BindingResult result, String addr_cd, String addr_nm) throws NoSuchAlgorithmException {
+        if(result.hasErrors())
             return "resisterForm";
-        }
-
         AddrCdDto addrCdDto = new AddrCdDto();
-
         addrCdDto.setUr_id(userDto.getId());
         addrCdDto.setAddr_cd(addr_cd);
         addrCdDto.setAddr_name(addr_nm);
-        addrCdDto.setState('Y');
-        addrCdDto.setFirst_date(new Timestamp(System.currentTimeMillis()));
-        addrCdDto.setFirst_id("admin");
-        addrCdDto.setLast_date(new Timestamp(System.currentTimeMillis()));
-        addrCdDto.setLast_id("admin");
-
         userService.insertNewUser(userDto, addrCdDto);
         return "loginForm";
-
     }
 
     @GetMapping(value = "/checkIdDuplication", produces = "text/plain;charset=UTF-8")
@@ -71,52 +62,18 @@ public class ResisterController {
     public String checkIdDuplication(@RequestParam("id") String id) {
         List<String> adminIdList = adminService.getAllAdminsId();
         List<String> AllUsersAdminsId = userService.getAllUsersAdminsId(adminIdList);
-
-        if(id.trim().length() != id.length()) {
-            return "아이디에 띄어쓰기를 넣을 수 없습니다.";
-        }
-        else if(hasMiddleSpace(id)) {
-            return "아이디에 띄어쓰기를 넣을 수 없습니다.";
-        }
-        else if(isNumeric(id)) {
-            return "아이디에는 반드시 영어와 숫자가 섞여있어야 합니다.";
-        }
-        else if(isAlphabetic(id)) {
-            return "아이디에는 반드시 영어와 숫자가 섞여있어야 합니다.";
-        }
-        else if(containsSpecialCharacter(id)) {
-            return "아이디에 한글 또는 특수문자는 불가능합니다.";
-        }
-        else {
-            for(int i=0; i<AllUsersAdminsId.size(); i++) {
-                if(id.equals(AllUsersAdminsId.get(i))) {
-                    return "이미 존재하는 아이디입니다.";
-                }
+        String answer=UserVaildation.vailed(id);
+        for(int i=0; i<AllUsersAdminsId.size(); i++) {
+            if(id.equals(AllUsersAdminsId.get(i))) {
+                answer = "이미 존재하는 아이디입니다.";
             }
         }
-
-        return "사용 가능한 아이디입니다.";
+        return answer;
     }
 
     @GetMapping(value = "/mediumCategory", produces = "application/json;charset=UTF-8")
     @ResponseBody
     public List<AdministrativeDto> mediumCategory(@RequestParam("tradingPlace_A_large") String largeCategory) throws Exception {
         return administrativeDao.selectMediumCategory(largeCategory);
-    }
-
-    public boolean hasMiddleSpace(String str) {
-        return str.matches(".+\\s+.+");
-    }
-
-    public boolean isNumeric(String str) {
-        return str.matches("^[0-9]+$");
-    }
-
-    public boolean isAlphabetic(String str) {
-        return str.matches("^[A-Za-z]+$");
-    }
-
-    public boolean containsSpecialCharacter(String str) {
-        return str.matches(".*[^A-Za-z0-9].*");
     }
 }
